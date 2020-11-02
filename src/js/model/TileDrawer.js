2a5aabb279ec25fa760eb493389a11181d707e33
Henrik Norman <honke.norman@gmail.com>
2020-11-02 09:54:26 +0100

    Batching draws, textures, & buffers

--- src/js/model/TileDrawer.js
+++ src/js/model/TileDrawer.js
@@ -1,11 +1,30 @@
 "use strict";
 
 import global from '../Global';
+import BatchOfTiles from './BatchOfTiles';
+
+//TODO dynamically set to utilize the most out of every hardware
+// console.log("gl.getParameter(gl.MAX_TEXTURE_IMAGE_UNITS); " + this.gl.getParameter(this.gl.MAX_TEXTURE_IMAGE_UNITS));
+// console.log("gl.getParameter(gl.MAX_VIEWPORT_DIMS); " + this.gl.getParameter(this.gl.MAX_VIEWPORT_DIMS));
+// console.log("gl.getParameter(gl.MAX_VERTEX_TEXTURE_IMAGE_UNITS); " + this.gl.getParameter(this.gl.MAX_VERTEX_TEXTURE_IMAGE_UNITS));
+const N_TILES_PER_ROW = 4;
+const N_TILES_PER_TEXTURE = N_TILES_PER_ROW * N_TILES_PER_ROW;
+const N_MAX_POSSIBLE_TEXTURES = Math.ceil(200 / N_TILES_PER_TEXTURE);
+const N_MAX_POSSIBLE_TILES_IN_MEMORY = N_MAX_POSSIBLE_TEXTURES * N_TILES_PER_TEXTURE;
 
 class TileDrawer {
 
 	constructor() {
 		this.tiles = {};
+		this.tileArray = [];
+		this.tilesWaitingToBeRemoved = {};
+		this.textureCoordinates = new Float32Array(8 * N_TILES_PER_TEXTURE * N_MAX_POSSIBLE_TEXTURES);
+		this.vertexIndices = new Uint16Array(6 * N_TILES_PER_TEXTURE * N_MAX_POSSIBLE_TEXTURES);
+		this.vertexPosition = new Float32Array(12 * N_TILES_PER_TEXTURE * N_MAX_POSSIBLE_TEXTURES);
+		this.nextIndex = 0;
+		this.batchOfTiles = [];
+		this.indiciesToReuse = [];
+		this.numberOfVisibleTiles = 0;
 	}
 
 	initShaders () {
@@ -80,10 +99,6 @@ class TileDrawer {
 	enableShader(pMatrix, vMatrix, modelMatrix){
 		this.gl.useProgram(this.shaderProgram);
 
-		this.shaderProgram.pMatrixUniform = this.gl.getUniformLocation(this.shaderProgram, "uPMatrix");
-		this.shaderProgram.mMatrixUniform = this.gl.getUniformLocation(this.shaderProgram, "uMMatrix");
-		this.shaderProgram.vMatrixUniform = this.gl.getUniformLocation(this.shaderProgram, "uVMatrix");
-		this.shaderProgram.samplerUniform = this.gl.getUniformLocation(this.shaderProgram, "uSampler0");
 		this.shaderProgram.uniformVertexTextureFactor = this.gl.getUniformLocation(this.shaderProgram, "uFactor0");
 		this.shaderProgram.sphericalGridEnabledUniform = this.gl.getUniformLocation(this.shaderProgram, "uSphericalGrid");
 
@@ -101,14 +116,47 @@ class TileDrawer {
 	init(){
 		this.gl = global.gl;
 		this.initShaders();
+		this.vertexPositionBuffer = this.gl.createBuffer();
+		this.vertexTextureCoordBuffer = this.gl.createBuffer();
+		this.vertexIndexBuffer = this.gl.createBuffer();
+
+		this.batchOfTiles.push(new BatchOfTiles(N_TILES_PER_ROW, 0, this.vertexPositionBuffer, this.vertexTextureCoordBuffer, this.vertexIndexBuffer));
+		this.gl.activeTexture(this.gl.TEXTURE0);
+			
+		this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, true);
+
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexPositionBuffer);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, this.vertexPosition, this.gl.DYNAMIC_DRAW);
+		this.vertexPositionBuffer.itemSize = 3;
+		this.vertexPositionBuffer.numItems = this.vertexPosition.length;
+		
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexTextureCoordBuffer);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, this.textureCoordinates, this.gl.DYNAMIC_DRAW);
+		this.vertexTextureCoordBuffer.itemSize = 2;
+		this.vertexTextureCoordBuffer.numItems = this.textureCoordinates.length;
+		
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
+		this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, this.vertexIndices, this.gl.DYNAMIC_DRAW);
+		this.vertexIndexBuffer.itemSize = 1;
+		this.vertexIndexBuffer.numItems = this.vertexIndices.length;
 	}
 
 	add(tile){
 		let tileKey = tile.order + "/" + tile.ipix;
 		if(!this.tiles[tileKey]){
-			this.tiles[tileKey] = tile; 
+			this.tiles[tileKey] = tile;
 			if(!tile.imageLoaded){
 				tile.startLoadingImage();
+			} else if(!tile.textureLoaded){
+				this.tileLoaded(tile);
+			} else {
+				//Still in buffer
+				let tileKey = tile.order + "/" + tile.ipix;
+				if(DEBUG){
+					console.log("Tile not fully removed yet - Using same index - Removing " + tileKey);
+				}
+				delete this.tilesWaitingToBeRemoved[tileKey];
+				this.indiciesToReuse = this.indiciesToReuse.filter(item => item !== tileKey);
 			}
 		}
 	}
@@ -119,40 +167,79 @@ class TileDrawer {
 		if(this.tiles[tileKey]){
 			if(!tile.imageLoaded){
 				tile.stopLoadingImage();
+			} else if(tile.textureLoaded && !this.tilesWaitingToBeRemoved[tileKey]) {
+				this.indiciesToReuse.push(tileKey);
+				tile.inListToBeRemoved = true;
+				this.tilesWaitingToBeRemoved[tileKey] = tile;
+				if(DEBUG){
+					console.log("Removing tile - Adding index " + tile.index + " to indiciesToReuse");
+				}
 			}
 			delete this.tiles[tileKey];
 		}
 	}
 
-	clear(){
-		this.tiles = {};
+	setTileIndex(tile){
+		if(this.nextIndex >= N_MAX_POSSIBLE_TILES_IN_MEMORY){
+			if(this.indiciesToReuse.length > 0){
+				let tileKeyToOverwrite = this.indiciesToReuse.shift();
+				let tileToRemove = this.tilesWaitingToBeRemoved[tileKeyToOverwrite];
+				tile.index = tileToRemove.index;
+				tileToRemove.textureLoaded = false;
+				this.numberOfVisibleTiles--;
+
+				delete this.tilesWaitingToBeRemoved[tileKeyToOverwrite];
+				//TODO get previous tile with that index and fully destruct it
+				if(DEBUG){
+					console.log("Reusing old index " + tile.index);
+				}
+			} else {
+				tile.index = this.nextIndex;
+				this.nextIndex++;
+				console.error("Buffers full ");
+				//TODO if this happens, there will be a buffer overflow since vertexPositionBuffer is not big enough
+			}
+		} else {
+			tile.index = this.nextIndex;
+			this.nextIndex++;
+			if(DEBUG){
+				console.log("NextIndex " + this.nextIndex);
+			}
+		}
+	}
+
+	tileLoaded(tile){
+		let tileKey = tile.order + "/" + tile.ipix;
+		if(!this.tiles[tileKey]){
+			return;
+		}
+		this.setTileIndex(tile);
+		this.tileArray[tile.index] = tile;
+		this.numberOfVisibleTiles++;
+		if(DEBUG){
+			console.log("numberOfVisibleTiles: " + this.numberOfVisibleTiles);
+		}
+		let batchIndex = Math.floor(tile.index / N_TILES_PER_TEXTURE);
+		if(this.batchOfTiles[batchIndex] == undefined){
+			this.batchOfTiles[batchIndex] = new BatchOfTiles(N_TILES_PER_ROW, batchIndex, this.vertexPositionBuffer, this.vertexTextureCoordBuffer, this.vertexIndexBuffer);
+		}
+		this.batchOfTiles[batchIndex].addTile(tile);
 	}
 
 	draw(pMatrix, vMatrix, modelMatrix){
 		this.enableShader(pMatrix, vMatrix, modelMatrix);
-		Object.keys(this.tiles).forEach(ipix => {
-			this.drawTile(this.tiles[ipix]);
-		});
-	}
-
-	drawTile(tile){
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, tile.vertexPositionBuffer);
-		this.gl.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, tile.vertexPositionBuffer.itemSize, this.gl.FLOAT, false, 0, 0);
-
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, tile.vertexTextureCoordBuffer);
-		this.gl.vertexAttribPointer(this.shaderProgram.textureCoordAttribute, tile.vertexTextureCoordBuffer.itemSize, this.gl.FLOAT, false, 0, 0);
-
-		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, tile.vertexIndexBuffer);
-
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexPositionBuffer);
+		this.gl.vertexAttribPointer(this.shaderProgram.vertexPositionAttribute, this.vertexPositionBuffer.itemSize, this.gl.FLOAT, false, 0, 0);
+		
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexTextureCoordBuffer);
+		this.gl.vertexAttribPointer(this.shaderProgram.textureCoordAttribute, this.vertexTextureCoordBuffer.itemSize, this.gl.FLOAT, false, 0, 0);
+		
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
+		
 		this.gl.enableVertexAttribArray(this.shaderProgram.vertexPositionAttribute);
 		this.gl.enableVertexAttribArray(this.shaderProgram.textureCoordAttribute);
-
-		this.gl.activeTexture(this.gl.TEXTURE0);
-		this.gl.bindTexture(this.gl.TEXTURE_2D, tile.tex);
-		// this.gl.uniform1f(uniformVertexTextureFactorLoc, this.opacity);
-
-		this.gl.drawElements(this.gl.TRIANGLES, 6,
-			this.gl.UNSIGNED_SHORT, 0);
+		this.batchOfTiles.forEach((batch) => batch.draw(this.shaderProgram.samplerUniform));
 	}
+
 }
 export const tileDrawerSingleton = new TileDrawer();
