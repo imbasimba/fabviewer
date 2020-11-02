644acd38527ce3336945c86c08f002c56a20ea7b
Henrik Norman <honke.norman@gmail.com>
2020-11-02 09:46:26 +0100

    Moved Texture handling from tile to tileDrawer

--- src/js/model/Tile.js
+++ src/js/model/Tile.js
@@ -10,7 +10,6 @@ class Tile {
 
 	constructor(order, ipix, radius) {
 		this.gl = global.gl;
-        this.shaderProgram = this.gl.program;
 		this.order = order;
 		this.ipix = ipix;
 		this.radius = radius != undefined ? radius : 1;
@@ -25,15 +24,13 @@ class Tile {
 		this.numberOfVisibleChildrenReadyToDraw = 0;
 		this.childrenReady = false;
 
-		this.opacity = 1.00 * 100.0/100.0;
-
 		this.initBuffer();
-		this.initTexture();
+		this.initImage();
 		this.children = [];
 	}
 
 	initBuffer () {
-		let vertexPosition = new Float32Array(12);
+		this.vertexPosition = new Float32Array(12);
 		let facesVec3Array = global.getHealpix(this.order).getBoundaries(this.ipix);
 		if (this.radius != 1){
 			// HiPS radius different from Healpix default radius 1.
@@ -50,102 +47,42 @@ class Tile {
 			phi2 = Math.atan2(facesVec3Array[2].y, facesVec3Array[2].x);
 			phi3 = Math.atan2(facesVec3Array[3].y, facesVec3Array[3].x);
 
-			vertexPosition[0] = -this.radius * Math.sin(theta0) * Math.cos(phi0);
-			vertexPosition[1] = this.radius * Math.sin(theta0) * Math.sin(phi0);
-			vertexPosition[2] = this.radius * Math.cos(theta0);
+			this.vertexPosition[0] = -this.radius * Math.sin(theta0) * Math.cos(phi0);
+			this.vertexPosition[1] = this.radius * Math.sin(theta0) * Math.sin(phi0);
+			this.vertexPosition[2] = this.radius * Math.cos(theta0);
 
-			vertexPosition[3] = -this.radius * Math.sin(theta1) * Math.cos(phi1);
-			vertexPosition[4] = this.radius * Math.sin(theta1) * Math.sin(phi1);
-			vertexPosition[5] = this.radius * Math.cos(theta1);
+			this.vertexPosition[3] = -this.radius * Math.sin(theta1) * Math.cos(phi1);
+			this.vertexPosition[4] = this.radius * Math.sin(theta1) * Math.sin(phi1);
+			this.vertexPosition[5] = this.radius * Math.cos(theta1);
 
-			vertexPosition[6] = -this.radius * Math.sin(theta2) * Math.cos(phi2);
-			vertexPosition[7] = this.radius * Math.sin(theta2) * Math.sin(phi2);
-			vertexPosition[8] = this.radius * Math.cos(theta2);
+			this.vertexPosition[6] = -this.radius * Math.sin(theta2) * Math.cos(phi2);
+			this.vertexPosition[7] = this.radius * Math.sin(theta2) * Math.sin(phi2);
+			this.vertexPosition[8] = this.radius * Math.cos(theta2);
 
-			vertexPosition[9] = -this.radius * Math.sin(theta3) * Math.cos(phi3);
-			vertexPosition[10] = this.radius * Math.sin(theta3) * Math.sin(phi3);
-			vertexPosition[11] = this.radius * Math.cos(theta3);
+			this.vertexPosition[9] = -this.radius * Math.sin(theta3) * Math.cos(phi3);
+			this.vertexPosition[10] = this.radius * Math.sin(theta3) * Math.sin(phi3);
+			this.vertexPosition[11] = this.radius * Math.cos(theta3);
 		} else{
-			vertexPosition[0] = facesVec3Array[0].x ;
-			vertexPosition[1] = facesVec3Array[0].y ;
-			vertexPosition[2] = facesVec3Array[0].z;
+			this.vertexPosition[0] = facesVec3Array[0].x ;
+			this.vertexPosition[1] = facesVec3Array[0].y ;
+			this.vertexPosition[2] = facesVec3Array[0].z;
 
-			vertexPosition[3] = facesVec3Array[1].x;
-			vertexPosition[4] = facesVec3Array[1].y;
-			vertexPosition[5] = facesVec3Array[1].z;
+			this.vertexPosition[3] = facesVec3Array[1].x;
+			this.vertexPosition[4] = facesVec3Array[1].y;
+			this.vertexPosition[5] = facesVec3Array[1].z;
 
-			vertexPosition[6] = facesVec3Array[2].x;
-			vertexPosition[7] = facesVec3Array[2].y;
-			vertexPosition[8] = facesVec3Array[2].z;
+			this.vertexPosition[6] = facesVec3Array[2].x;
+			this.vertexPosition[7] = facesVec3Array[2].y;
+			this.vertexPosition[8] = facesVec3Array[2].z;
 
-			vertexPosition[9] = facesVec3Array[3].x;
-			vertexPosition[10] = facesVec3Array[3].y;
-			vertexPosition[11] = facesVec3Array[3].z;
+			this.vertexPosition[9] = facesVec3Array[3].x;
+			this.vertexPosition[10] = facesVec3Array[3].y;
+			this.vertexPosition[11] = facesVec3Array[3].z;
 		}
+	}
 
-		var textureCoordinates = new Float32Array(8);
-
-		// UV mapping: 1, 0],[1, 1],[0, 1],[0, 0]
-		textureCoordinates[0] = 1.0;
-		textureCoordinates[1] = 0.0;
-		textureCoordinates[2] = 1.0;
-		textureCoordinates[3] = 1.0;
-		textureCoordinates[4] = 0.0;
-		textureCoordinates[5] = 1.0;
-		textureCoordinates[6] = 0.0;
-		textureCoordinates[7] = 0.0;
-
-	    var vertexIndices = new Uint16Array(6);
-	    var baseFaceIndex = 0;
-		vertexIndices[0] = baseFaceIndex;
-		vertexIndices[1] = baseFaceIndex + 1;
-		vertexIndices[2] = baseFaceIndex + 2;
-
-		vertexIndices[3] = baseFaceIndex;
-		vertexIndices[4] = baseFaceIndex + 2;
-		vertexIndices[5] = baseFaceIndex + 3;
-
-		baseFaceIndex = baseFaceIndex + 4;
-
-	    this.vertexPositionBuffer = this.gl.createBuffer();
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexPositionBuffer);
-		this.gl.bufferData(this.gl.ARRAY_BUFFER, vertexPosition, this.gl.STATIC_DRAW);
-		this.vertexPositionBuffer.itemSize = 3;
-		this.vertexPositionBuffer.numItems = vertexPosition.length;
-
-		this.vertexTextureCoordBuffer = this.gl.createBuffer();
-		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexTextureCoordBuffer);
-		this.gl.bufferData(this.gl.ARRAY_BUFFER, textureCoordinates, this.gl.STATIC_DRAW);
-		this.vertexTextureCoordBuffer.itemSize = 2;
-		this.vertexTextureCoordBuffer.numItems = textureCoordinates.length;
-
-		this.vertexIndexBuffer = this.gl.createBuffer();
-	    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.vertexIndexBuffer);
-	    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, vertexIndices, this.gl.STATIC_DRAW);
-	    this.vertexIndexBuffer.itemSize = 1;
-		this.vertexIndexBuffer.numItems = vertexIndices.length;
-	};
-
-	initTexture () {
-		this.numberOfLoadedImages = 0;
-		this.isFullyLoaded = false;
-
-		if(this.image){ // Image is currently loading or has been loaded previously
-			if(this.textureLoaded){
-				this.numberOfLoadedImages++;
-				this.isFullyLoaded = true;
-			} else if(this.imageLoaded){
-				this.handleLoadedTexture(this, 0);
-			}
-			return;
-		}
-		this.tex = this.gl.createTexture();
+	initImage () {
 		this.image = new Image();
-
-		// binding fake black image until the real image has been loaded (https://stackoverflow.com/questions/19722247/webgl-wait-for-texture-to-load/19748905#19748905)
-		this.gl.bindTexture(this.gl.TEXTURE_2D, this.tex);
-		this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, 1, 1, 0, this.gl.RGBA, this.gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 0, 0]));
-
 		var dirNumber = Math.floor(this.ipix / 10000) * 10000;
 
 		this.addOnLoad();
@@ -155,48 +92,15 @@ class Tile {
 		this.image.setAttribute('crossorigin', 'anonymous');
 		this.imageUrl = "https://skies.esac.esa.int/DSSColor/Norder"+this.order+"/Dir"+dirNumber+"/Npix"+this.ipix+fileFormat;
 	}
-
+	
 	addOnLoad(){
 		this.image.onload = ()=> {
 			this.imageLoaded = true;
-			this.handleLoadedTexture(0);
+			tileDrawerSingleton.tileLoaded(this);
+			if(this.parent){
+				this.parent.childReady();
+			}
 		};
-	}
-
-	handleLoadedTexture (shaderSkyIndex){
-		this.textureLoaded = true;
-		this.numberOfLoadedImages++;
-		this.isFullyLoaded = true;
-		this.gl.activeTexture(this.gl.TEXTURE0 + shaderSkyIndex);
-		this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, true);
-		this.gl.bindTexture(this.gl.TEXTURE_2D, this.tex);
-
-		try{
-			this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, this.image);
-		}catch(error){
-			console.error("ERROR");
-			console.error(error);
-			console.error("ipix: " + this.ipix);
-		}
-
-
-		this.gl.generateMipmap(this.gl.TEXTURE_2D);
-		// TODO check which mipmap filtering is better. The one active or the commented alternative
-		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR_MIPMAP_LINEAR);
-//	DO NOT DELETEthis.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
-//	DO NOT DELETEthis.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
-
-		// TODO REVIEW uniformSamplerLoc[shaderSkyIndex] !!!
-		// this.gl.uniform1i(this.shaderProgram.samplerUniform, shaderSkyIndex);
-
-		// if (!this.gl.isTexture(tile.tex)){
-		// 	console.log("error in texture");
-		// }
-
-		this.gl.bindTexture(this.gl.TEXTURE_2D, null);
-		if(this.parent){
-			this.parent.childReady();
-		}
 	}
 
 	startLoadingImage(){
@@ -210,6 +114,7 @@ class Tile {
 			this.image.src = this.imageUrl;
 		}
 	}
+
 	stopLoadingImage(){
 		this.image.src = "";
 	}
@@ -235,6 +140,17 @@ class Tile {
 		healpixGridTileDrawerSingleton.add(healpixGridTileBufferSingleton.getTile(this.order, this.ipix));
 	}
 
+	removeFromView(){
+		if(!this._isInView) {return}
+		this._isInView = false;
+
+		tileDrawerSingleton.remove(this);
+		healpixGridTileDrawerSingleton.remove(healpixGridTileBufferSingleton.getTile(this.order, this.ipix));
+		if(this.parent){
+			this.parent.childRemovedFromView();
+		}
+	}
+
 	childReady(){
 		this.numberOfVisibleChildrenReadyToDraw++;
 		if(this.numberOfVisibleChildrenReadyToDraw == 4 && global.order > this.order){
@@ -247,14 +163,14 @@ class Tile {
 		tileDrawerSingleton.remove(this);
 		healpixGridTileDrawerSingleton.remove(healpixGridTileBufferSingleton.getTile(this.order, this.ipix));
 		if(this.parent){
-			this.parent.childRemovedChildrenDrawnInstead();
+			this.parent.childRemovedSinceItsChildrenDrawnInstead();
 		}
 	}
 
-	childRemovedChildrenDrawnInstead(){
+	childRemovedSinceItsChildrenDrawnInstead(){
 		let drawnChildren = 0;
 		this.children.forEach(child => {
-			if((child._isInView && child.textureLoaded && global.order > this.order) 
+			if((child._isInView && child.imageLoaded && global.order > this.order) 
 				|| (child.childrenReady && global.order > child.order) 
 				){
 					drawnChildren++;
@@ -272,7 +188,7 @@ class Tile {
 			if(child.isInView()){ 
 				numberOfVisibleChildren++;
 			}
-			if(child.isInView() && child.textureLoaded){ 
+			if(child.isInView() && child.imageLoaded){ 
 				numberOfChildrenInViewWithLoadedTextures++;
 			}
 		});
@@ -285,17 +201,6 @@ class Tile {
 		}
 	}
 
-	removeFromView(){
-		if(!this._isInView) {return}
-		this._isInView = false;
-
-		tileDrawerSingleton.remove(this);
-		healpixGridTileDrawerSingleton.remove(healpixGridTileBufferSingleton.getTile(this.order, this.ipix));
-		if(this.parent){
-			this.parent.childRemovedFromView();
-		}
-	}
-
 	childRemovedFromView(){
 		let numberOfVisibleChildren = 0;
 		let numberOfChildrenInViewWithLoadedTextures = 0;
@@ -303,7 +208,7 @@ class Tile {
 			if(child.isInView()){ 
 				numberOfVisibleChildren++;
 			}
-			if(child.isInView() && child.textureLoaded){ 
+			if(child.isInView() && child.imageLoaded){ 
 				numberOfChildrenInViewWithLoadedTextures++;
 			}
 		});
